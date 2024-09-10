861be35909c71dd8bef035781187a00ce1a53d4e
jimmygust <jimgus-7@student.ltu.se>
2024-09-10 13:55:14 +0200

    Working meny

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,20 @@
 package org.example;
 
-public class App {
+import java.util.Scanner;
 
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        Scanner scanner = new Scanner(System.in);
+
+        String meny = """
+                Elpriser
+                ========
+                1. Inmatning
+                2. Min, Max och Medel
+                3. Sortera
+                4. Bästa Laddningstid (4h)
+                e. Avsluta
+                """;
+        System.out.println(meny);
     }
 }
